d8587c754f2d4d7f98dbeb367ed3e5281e4bac96
Djabiri Abalkassim <abalkass@hotmail.fr>
2017-01-27 08:57:51 +0100

    Commencement du DAO xml

---
+++ src/dao/CatalogueDAO_XML.java
@@ -0,0 +1,129 @@
+package dao;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+
+import metier.I_Catalogue;
+import metier.I_Produit;
+import metier.Produit;
+
+public class CatalogueDAO_XML implements I_DAO<I_Catalogue> {
+	private String uri = "Catalogues.xml";
+	private Document doc;
+
+	public CatalogueDAO_XML() {
+		SAXBuilder sdoc = new SAXBuilder();
+		try {
+			doc = sdoc.build(uri);
+		} catch (Exception e) {
+			System.out.println("erreur construction arbre JDOM");
+		}
+	}
+
+	@Override
+	public boolean create(I_Catalogue c) {
+		// TODO Stub de la méthode généré automatiquement
+		try {
+			Element root = doc.getRootElement();
+			Element prod = new Element("produit");
+			prod.setAttribute("nom", c.getNom());
+			Element prix = new Element("prixHT");
+			prod.addContent(prix.setText(String.valueOf(p.getPrixUnitaireHT())));
+			Element qte = new Element("quantite");
+			prod.addContent(qte.setText(String.valueOf(p.getQuantite())));
+			root.addContent(prod);
+			return sauvegarde();
+		} catch (Exception e) {
+			System.out.println("erreur creer produit");
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(String nomT, int qte) throws QuantiteeStock_Exception {
+		try {
+			Element prod = chercheProduit(p.getNom());
+			if (prod != null) {
+				prod.getChild("quantite").setText(String.valueOf(p.getQuantite()));
+				return sauvegarde();
+			}
+			return false;
+		} catch (Exception e) {
+			System.out.println("erreur maj produit");
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(String nomT) {
+		try {
+			Element root = doc.getRootElement();
+			Element prod = chercheProduit(p.getNom());
+			if (prod != null) {
+				root.removeContent(prod);
+				return sauvegarde();
+			} else
+				return false;
+		} catch (Exception e) {
+			System.out.println("erreur supprimer produit");
+			return false;
+		}
+	}
+
+	@Override
+	public List<I_Catalogue> find() {
+
+		List<I_Catalogue> l = new ArrayList<I_Catalogue>();
+		try {
+			Element root = doc.getRootElement();
+			List<Element> lProd = root.getChildren("produit");
+
+			for (Element prod : lProd) {
+				String nomP = prod.getAttributeValue("nom");
+				Double prix = Double.parseDouble(prod.getChild("prixHT").getText());
+				int qte = Integer.parseInt(prod.getChild("quantite").getText());
+				l.add(new Produit(nomP, prix, qte));
+			}
+		} catch (Exception e) {
+			System.out.println("erreur lireTous tous les produits");
+		}
+		return l;
+	}
+
+	@Override
+	public void deconnexion() {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	private boolean sauvegarde() {
+		System.out.println("Sauvegarde");
+		XMLOutputter out = new XMLOutputter();
+		try {
+			out.output(doc, new PrintWriter(uri));
+			return true;
+		} catch (Exception e) {
+			System.out.println("erreur sauvegarde dans fichier XML");
+			return false;
+		}
+	}
+
+	private Element chercheProduit(String nom) {
+		Element root = doc.getRootElement();
+		List<Element> lProd = root.getChildren("produit");
+		int i = 0;
+		while (i < lProd.size() && !lProd.get(i).getAttributeValue("nom").equals(nom))
+			i++;
+		if (i < lProd.size())
+			return lProd.get(i);
+		else
+			return null;
+	}
+
+}
